6a2d51058ec800fb4f812dac29f36460d4cd02a6
sylaaang <mehdi.hachem@yutopia.io>
2024-10-23 00:06:26 +0200

    feat planet 3D

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import ParticlesBackgroundClient from './components/ParticlesBackgroundClient';
+import CustomCanvas from './components/CustomCanvas';
 
 export default function Home() {
   return (
     <>
        <ParticlesBackgroundClient />
       <main>
-
+      <CustomCanvas />
       </main>
     </>
   );
