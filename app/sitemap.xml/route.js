662063b3f8e77d3f821fc80fcfc4da9d93b50e26
sylaaang <mehdi.hachem@yutopia.io>
2025-05-27 19:38:10 +0200

    feat sitemap.xml

--- app/sitemap.xml/route.js
+++ app/sitemap.xml/route.js
@@ -1,15 +1,15 @@
 export async function GET() {
   const baseUrl = process.env.NEXT_PUBLIC_SITE_URL;
 
-  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    <url>
-      <loc>${baseUrl}/</loc>
-      <lastmod>${new Date().toISOString()}</lastmod>
-      <changefreq>monthly</changefreq>
-      <priority>1.0</priority>
-    </url>
-  </urlset>`;
+const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>${baseUrl}/</loc>
+    <lastmod>${new Date().toISOString()}</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>1.0</priority>
+  </url>
+</urlset>`;
 
   return new Response(sitemap, {
     status: 200,
