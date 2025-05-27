326d20de0cac6e601777f35d24f955521ec2bdad
sylaaang <mehdi.hachem@yutopia.io>
2025-05-27 18:52:57 +0200

    feat: ajouter un endpoint dynamique pour robots.txt avec URL sitemap basé sur NEXT_PUBLIC_SITE_URL
    - Création de la route API obots.txt dans pp/robots.txt/route.js
    - Génération dynamique du fichier robots.txt incluant l’URL complète du sitemap à partir de la variable d’environnement NEXT_PUBLIC_SITE_URL
    - Facilite la découverte du sitemap par les moteurs de recherche et améliore le SEO

---
+++ app/sitemap.xml/route.js
@@ -0,0 +1,20 @@
+// app/sitemap.xml/route.js
+export async function GET() {
+  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL;
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+      <loc>${baseUrl}/</loc>
+      <lastmod>${new Date().toISOString()}</lastmod>
+      <changefreq>monthly</changefreq>
+      <priority>1.0</priority>
+    </url>
+  </urlset>`;
+
+  return new Response(sitemap, {
+    headers: {
+      "Content-Type": "application/xml",
+    },
+  });
+}
